f79d071840e56d9fb7c4595f25330038e7800ee4
sergiyhelp <sergiyhelp@gmail.com>
2021-03-23 20:56:18 +0200

    update config

--- config.h
+++ config.h
@@ -2,13 +2,11 @@
 #define delimiter " "
 
 static const Block blocks[] = {
-	{"sb-mail", 1800, 1},
-	{"sb-music", 0, 2},
-	{"sb-disk", 1800, 4},
-	{"sb-memory", 10, 3},
-	{"sb-loadavg", 10, 9},
-	{"sb-volume", 0, 5},
-	{"sb-battery", 5, 6},
-	{"sb-date", 60, 7},
-	{"sb-network", 5, 8},
+	/*Command*/	/*Update Interval*/	/*Update Signal*/
+	{ "status-locale",      0,	     9},
+	{ "status-backlight",   0,	     8},
+	{ "status-volume",      0,	     1},
+	{ "status-power",       10,	    11},
+	{ "status-date",	    30,	    12},
+	{ "status-time",	    30,	    10},
 };
